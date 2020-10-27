522662b7e44af9d3b6e2380d581bce09837a5b77
Valerii Pushkar <valerii.pushkar.work@gmail.com>
2020-10-27 12:35:39 +0100

    first commit

---
+++ plopfile.js
@@ -0,0 +1,18 @@
+'use strict';
+const path = require('path');
+module.exports = function (plop) {
+    // controller generator
+    plop.setGenerator('controller', {
+        description: 'application controller logic',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'controller name please'
+        }],
+        actions: [{
+            type: 'add',
+            path: 'src/{{name}}.js',
+            templateFile: 'plop-templates/controller.ts'
+        }]
+    });
+};
